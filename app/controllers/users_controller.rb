c90a51979db7d7910af2b5c5290759528230f195
Jared Dietz <jaredsurya@gmail.com>
2023-01-16 20:22:19 -0500

    added routes to routes dot rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.create!(user_params)
+    login_user
+    render json: @user, status: 201
+  end
+
+  def show
+    if current_user
+      render json: current_user, status: 201
+    else 
+      render json: { error: "Invalid Username or Password" }, status: 401
+    end
+  end
+
+  def index
+    users = User.all
+    render json: users
+  end 
+
+  private
+
+  def user_params 
+    params.permit(:username, :first_name, :password, :password_confirmation)
+  end
+
 end
